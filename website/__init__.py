ada5000e61300e4f8e8f964a1a334765327b40e8
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-13 00:41:34 +0000

    Got user database to update
    Now added ability to add users to database to sign up and also log in but need to add actual authentication checks

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+import os
 from flask_login import LoginManager
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy()
-DB_NAME = "database.db"
+
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'blahblah'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "database.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
 
     from .views import views
@@ -22,7 +25,7 @@ def create_app():
 
     from .dbmodels import User, Note, Flashcard
 
-    if not path.exists('website/' + DB_NAME):
+    if not os.path.exists(os.path.join(basedir, "database.db")):
       with app.app_context():
         db.create_all()
     
--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,25 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash,redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from .dbmodels import User
+from . import views
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login', methods = ['GET','POST'])
 def login():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email = email).first()
+    if user:
+      if check_password_hash(user.password, password):
+        flash('You have successfully logged in', category ='success')
+        return redirect(url_for('views.home'))
+      else:
+        flash('Incorrect password', category = 'error')
+    else:
+      flash('Incorrect email', category = 'error')
   return render_template("login.html")
 
 @auth.route('/logout')
@@ -19,7 +35,10 @@ def sign_up():
     password = request.form.get('password')
     password2 = request.form.get('password2')
 
-    if len(email)<4:
+    user = User.query.filter_by(email = email).first()
+    if user:
+      flash('Account already exists', category = 'error')  
+    elif len(email)<4:
       flash('Email must be greater than 3 characters.', category = 'error') #category assignment used to pass into html
     elif len(first_name)<2:
       flash('First name must be greater than 1 character.', category = 'error')
@@ -28,7 +47,13 @@ def sign_up():
     elif password != password2:
       flash('Passwords dont match', category = 'error')
     else:
+      new_user = User(email = email, first_name = first_name, password = generate_password_hash(password,method='scrypt', salt_length=16))
+      db.session.add(new_user)
+      db.session.commit()
       flash('Account created' , category = 'success')
+
+      return redirect(url_for('auth.login'))
+      
       
   
   return render_template("sign-up.html")
