869bb88561129432710020c83715b0ee17a9af5e
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-16 10:22:03 +0000

    added functionality to detect which button is pressed and then change response based on this

--- website/views.py
+++ website/views.py
@@ -13,9 +13,9 @@ def home():
   return render_template("home.html", user=current_user)
 
 @views.route('/try-it-out', methods = ['GET','POST'])
-def try_it_out():
+def try_it_out(): 
   if request.method == 'POST':
-    if request.form.get('submit_button') == 'create_flashcard':
+    if request.form.get('submit_button') == 'create_flashcard': 
       prompt = request.form.get('note')
       if len(prompt) >= 300:
         flash('Note must be less than 300 characters', category = "error")
@@ -28,9 +28,16 @@ def try_it_out():
           #index 3 is incorrect answer 2
           #index 4 is incorrect answer 3
         question = flashcard[0]
-        answers = [(flashcard[1],True), (flashcard[2], False), (flashcard[3], False), (flashcard[4], False)]
+        correct_answer = flashcard[1]
+        answers = [flashcard[1], flashcard[2], flashcard[3], flashcard[4]]
         random.shuffle(answers)
-        return render_template("try_it_out.html", user=current_user, question=question, answers=answers)
+        return render_template("try_it_out.html", user=current_user, question=question, answers=answers, correct_answer=correct_answer)
+    elif request.form.get('submit_button') == 'submit_answer':
+      print(request.form)
+      answer = request.form.get('answer')
+      correct_answer = request.form.get('correct_answer')
+      if answer == correct_answer:
+        pass:
     
   
   
